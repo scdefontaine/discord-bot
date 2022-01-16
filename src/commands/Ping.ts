ac42e0673032e1eea1b79c57b191672645d4121e
Sean <scd13d@gmail.com>
2022-01-15 19:43:48 -0500

    adding basic error handling

--- src/commands/Ping.ts
+++ src/commands/Ping.ts
@@ -4,7 +4,11 @@ export const Ping: CommandInterface = {
   name: "ping",
   desc: "A simple ping pong to ensure bot is running",
   run: async (message) => {
-    const { channel } = message;
-    await channel.send("pong");
+    try {
+      const { channel } = message;
+      await channel.send("pong");
+    } catch (err) {
+      console.log("", err);
+    }
   }
 }
